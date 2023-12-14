641495c67b32488457c80dfc0ea22daa277baae2
breaun1 <breaun1@gmail.com>
2023-12-13 20:54:13 -0600

    getting the setup, setup

---
+++ script.js
@@ -0,0 +1,13 @@
+const body = document.querySelector("body")
+const h2Var = document.createElement("h2");
+const sec1 = document.createElement("section");
+const footer = document.createElement("footer");
+
+body.appendChild(h2Var);
+body.appendChild(sec1);
+body.appendChild(footer);
+
+
+h2Var.classList.add = ("h2");
+h2Var.textContent = "Hello";
+
